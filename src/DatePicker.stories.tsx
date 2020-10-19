d14ba0f3d9f8073be4ecb47abf3ccd9d62d0f9e8
Johan Bergström <johan.bergstrom@isotop.se>
2020-10-19 15:13:39 +0200

    add exports from index.ts

--- src/DatePicker.stories.tsx
+++ src/DatePicker.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
-import DatePicker, { DatePickerProps } from "./DatePicker";
+import DatePicker, { DatePickerProps } from ".";
 import { enUS } from "date-fns/locale";
 import { addYears } from "date-fns";
 
